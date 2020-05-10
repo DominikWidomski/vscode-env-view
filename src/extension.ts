bf9bc9ed56e02c7aee71ce1a8d8d3a34975fb68b
Dominik Widomski <dwidomski92@gmail.com>
2020-05-10 17:39:55 +0100

    style: format whole repo

--- src/extension.ts
+++ src/extension.ts
@@ -1,110 +1,122 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-import * as path from 'path';
-import * as fs from 'fs';
+import * as vscode from "vscode";
+import * as path from "path";
+import * as fs from "fs";
 import parse from "./parse";
-import { DefaultSerializer, DefaultDeserializer } from 'v8';
 
 const findEnvFiles = () => {
-	if(!vscode.workspace.rootPath) {
-		console.log('No workspace.rootPath');
-		return;
-	}
-	
-	const files = fs.readdirSync(vscode.workspace.rootPath).filter(file => {
-		if (file.startsWith('.env')) {
-			console.log(`Found file: ${file}`);
-			return true;
-		}
+  if (!vscode.workspace.rootPath) {
+    console.log("No workspace.rootPath");
+    return;
+  }
 
-		return false;
-	});
+  const files = fs.readdirSync(vscode.workspace.rootPath).filter((file) => {
+    if (file.startsWith(".env")) {
+      console.log(`Found file: ${file}`);
+      return true;
+    }
 
-	console.log(`Found ${files.length} files!`);
-	console.log(files);
+    return false;
+  });
 
-	return files || [];
+  console.log(`Found ${files.length} files!`);
+  console.log(files);
+
+  return files || [];
 };
 
 const getFileContents = (file: string) => {
-	if(!vscode.workspace.rootPath) {
-		console.log('No workspace.rootPath');
-		return;
-	}
-	
-	return fs.readFileSync(path.resolve(vscode.workspace.rootPath, file));
+  if (!vscode.workspace.rootPath) {
+    console.log("No workspace.rootPath");
+    return;
+  }
+
+  return fs.readFileSync(path.resolve(vscode.workspace.rootPath, file));
 };
 
 // this method is called when your extension is activated
 // this method is called only once for the activation.
 // your extension is activated the very first time an activationEvent is triggered
 export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "env-ui" is now active!');
+  console.log(
+    `Extension is running in the workspace: ${vscode.workspace.name} @ ${vscode.workspace.rootPath}`
+  );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "env-ui" is now active!');
-	console.log(`Extension is running in the workspace: ${vscode.workspace.name} @ ${vscode.workspace.rootPath}`);
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand("env-ui.helloWorld", () => {
+    // The code you place here will be executed every time your command is executed
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('env-ui.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+    // Display a message box to the user
+    vscode.window.showInformationMessage("Hello World from env-ui!");
+  });
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from env-ui!');
-	});
+  context.subscriptions.push(disposable);
 
-	context.subscriptions.push(disposable);
+  let envViewPanel: vscode.WebviewPanel;
 
-	let envViewPanel: vscode.WebviewPanel;
-
-	context.subscriptions.push(vscode.commands.registerCommand("env-ui.showView", () => {
-		const columnToShowIn = vscode.window.activeTextEditor
+  context.subscriptions.push(
+    vscode.commands.registerCommand("env-ui.showView", () => {
+      const columnToShowIn = vscode.window.activeTextEditor
         ? vscode.window.activeTextEditor.viewColumn
-				: undefined;
-		const files = findEnvFiles();
+        : undefined;
+      const files = findEnvFiles();
 
-		const perFileHTML = files?.map(file => {
-			const contents = getFileContents(file);
+      const perFileHTML = files
+        ?.map((file) => {
+          const contents = getFileContents(file);
 
-			if(!contents) {
-				return '';
-			}
-			
-			// Can I use a more generic file parser? I need an AST to understand comments etc?
-			// I mean custom parser would do, because AST migth be overengineered.
-			const vars = parse(contents);
-			
-			// TODO: move HTML files into separate folder. How can I template these in a decent way?
-			return `
-				<h3>${file}</h3>
-				${
-					Object.entries(vars).length === 0 ?
-					"<i>File is empty</i>" :
-					`
-					<ul>
-						${Object.entries(vars)?.map(([key, value]) => {
-							return `<li><label><input type="checkbox" data-key="${key}"/ data-value="${value}">${key}: ${value}</label></li>`;
-						}).join('')}
-					</ul>	
-					`
-				}
-			`;
-		}).join('');
+          if (!contents) {
+            return "";
+          }
 
-		envViewPanel = vscode.window.createWebviewPanel(
-			'env-ui-view',
-			'Environment file UI',
-			columnToShowIn || vscode.ViewColumn.One, // is this necessary?
-			{
-				enableScripts: true
-			}
-		);
-		
-		// Don't really want to do that, necessarily, can we message the view with new data?
-		envViewPanel.webview.html = `
+          // Can I use a more generic file parser? I need an AST to understand comments etc?
+          // I mean custom parser would do, because AST migth be overengineered.
+          const vars = parse(contents);
+
+          // TODO: move HTML files into separate folder. How can I template these in a decent way?
+          return `
+            <h3>${file}</h3>
+            ${
+              Object.entries(vars).length === 0
+                ? "<i>File is empty</i>"
+                : `
+                  <ul>
+                    ${Object.entries(vars)
+                      ?.map(([key, value]) => {
+                        return `
+                          <li>
+                            <label>
+                              <input type="checkbox" data-key="${key}" data-value="${value}" />
+                              ${key}: ${value}
+                            </label>
+                          </li>
+                        `;
+                      })
+                      .join("")}
+                  </ul>	
+                `
+            }
+          `;
+        })
+        .join("");
+
+      envViewPanel = vscode.window.createWebviewPanel(
+        "env-ui-view",
+        "Environment file UI",
+        columnToShowIn || vscode.ViewColumn.One, // is this necessary?
+        {
+          enableScripts: true,
+        }
+      );
+
+      // Don't really want to do that, necessarily, can we message the view with new data?
+      envViewPanel.webview.html = `
 			<!DOCTYPE html>
 			<html lang="en">
 			<head>
@@ -138,34 +150,37 @@ export function activate(context: vscode.ExtensionContext) {
 			</html>
 		`;
 
-		envViewPanel.onDidDispose(
-			() => {
-				console.log("View was closed!");
-			},
-			null,
-			context.subscriptions
-		);
+      envViewPanel.onDidDispose(
+        () => {
+          console.log("View was closed!");
+        },
+        null,
+        context.subscriptions
+      );
 
-		envViewPanel.webview.onDidReceiveMessage(message => {
-			switch (message.command) {
-				case 'updateVariable':
-					console.log(`Update variable ${message.payload.key}: ${message.payload.value} (${message.payload.checkboxValue})`);
-					// TODO: Update the file... HOW? How do you update the files? Should I read the file and keep a record of where the values are in it? D:! UMMM!!!
-					// What if file changes when this is open? Can I listen for file changes?
-					return;
-				
-				default:
-					console.log(`Unrecognised message command: ${message.command}`);
-			}
-		});
-		
-		// Show the view if it already exists
-		// Yeh want to show the panel if it already exists, but then need to reload the files basically.
-		if(envViewPanel) {
-			envViewPanel.reveal(columnToShowIn);
-			return;
-		}
-	}));
+      envViewPanel.webview.onDidReceiveMessage((message) => {
+        switch (message.command) {
+          case "updateVariable":
+            console.log(
+              `Update variable ${message.payload.key}: ${message.payload.value} (${message.payload.checkboxValue})`
+            );
+            // TODO: Update the file... HOW? How do you update the files? Should I read the file and keep a record of where the values are in it? D:! UMMM!!!
+            // What if file changes when this is open? Can I listen for file changes?
+            return;
+
+          default:
+            console.log(`Unrecognised message command: ${message.command}`);
+        }
+      });
+
+      // Show the view if it already exists
+      // Yeh want to show the panel if it already exists, but then need to reload the files basically.
+      if (envViewPanel) {
+        envViewPanel.reveal(columnToShowIn);
+        return;
+      }
+    })
+  );
 }
 
 // this method is called when your extension is deactivated
